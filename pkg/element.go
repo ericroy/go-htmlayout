f8ce51809e030c026584a09cb397b79372f45500
Eric Roy <ericsroy@gmail.com>
2011-12-31 16:24:57 -0800

    Getting a little farther with cgo

--- pkg/element.go
+++ pkg/element.go
@@ -1,44 +1,53 @@
 package htmlayout
 /*
-#cgo CFLAGS: -I../htmlayout/include
+#cgo CFLAGS: -std=gnu89 -I../htmlayout/include
+//#cgo LDFLAGS: -l../htmlayout/lib/HTMLayout.lib
 #include <htmlayout.h>
-#include <htmlayout_dom.h>
 */
 import "C"
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 const (
-
-	// HLDOM_RESULT values
-	HLDOM_OK = 0
-	HLDOM_INVALID_HWND = 1 
-	HLDOM_INVALID_HANDLE = 2
-	HLDOM_PASSIVE_HANDLE = 3
-	HLDOM_INVALID_PARAMETER = 4
-	HLDOM_OPERATION_FAILED = 5
+	HLDOM_OK = C.HLDOM_OK
+	HLDOM_INVALID_HWND = C.HLDOM_INVALID_HWND
+	HLDOM_INVALID_HANDLE = C.HLDOM_INVALID_HANDLE
+	HLDOM_PASSIVE_HANDLE = C.HLDOM_PASSIVE_HANDLE
+	HLDOM_INVALID_PARAMETER = C.HLDOM_INVALID_PARAMETER
+	HLDOM_OPERATION_FAILED = C.HLDOM_OPERATION_FAILED
 	HLDOM_OK_NOT_HANDLED = -1
 )
 
-// Essentially just typedefs
-type DomResult C.HLDOM_RESULT
-
-var errorToString = make(map[DomResult]string)
-
 func init() {
-	errorToString[HLDOM_OK] = "HLDOM_OK"
-	errorToString[HLDOM_INVALID_HWND] = "HLDOM_INVALID_HWND"
-	errorToString[HLDOM_INVALID_HANDLE] = "HLDOM_INVALID_HANDLE"
-	errorToString[HLDOM_PASSIVE_HANDLE] = "HLDOM_PASSIVE_HANDLE"
-	errorToString[HLDOM_INVALID_PARAMETER] = "HLDOM_INVALID_PARAMETER"
-	errorToString[HLDOM_OPERATION_FAILED] = "HLDOM_OPERATION_FAILED"
-	errorToString[HLDOM_OK_NOT_HANDLED] = "HLDOM_OK_NOT_HANDLED"
 }
 
-func hlpanic( dr DomResult, msg string ) {
-	panic( fmt.Sprintf( "%s: %s", errorToString[dr], msg ) )
+
+type DomResult int
+
+var errorToString = map[DomResult]string {
+	HLDOM_OK: "HLDOM_OK",
+	HLDOM_INVALID_HWND: "HLDOM_INVALID_HWND",
+	HLDOM_INVALID_HANDLE: "HLDOM_INVALID_HANDLE",
+	HLDOM_PASSIVE_HANDLE: "HLDOM_PASSIVE_HANDLE",
+	HLDOM_INVALID_PARAMETER: "HLDOM_INVALID_PARAMETER",
+	HLDOM_OPERATION_FAILED: "HLDOM_OPERATION_FAILED",
+	HLDOM_OK_NOT_HANDLED: "HLDOM_OK_NOT_HANDLED",
+}
+
+type DomError struct {
+	Result DomResult
+	Message string
+}
+
+func (self DomError) String() string {
+	fmt.Sprintf( "%s: %s", errorToString[self.Result], self.Message )
+}
+
+func DomPanic(result DomResult, message string) {
+	panic(DomError{result, message})
 }
 
 
@@ -48,18 +57,19 @@ Handle
 Simple wrapper around an HELEMENT, provides reference 
 counting functionality
 */
-type Handle C.HELEMENT
 
-func (h Handle) use() {
-	if dr := HTMLayout_UseElement( h ); dr != HLDOM_OK {
-		hlpanic( dr, "UseElement" );
+type Handle unsafe.Pointer
+
+func (self Handle) use() {
+	if dr := C.HTMLayout_UseElement( self ); dr != HLDOM_OK {
+		DomPanic( dr, "UseElement" );
 	}
 }
 
-func (h Handle) unuse() {
-	if h {
-		if dr := HTMLayout_UnuseElement( h ); dr != HLDOM_OK {
-			hlpanic( dr, "UnuseElement" );
+func (self Handle) unuse() {
+	if self {
+		if dr := C.HTMLayout_UnuseElement( self ); dr != HLDOM_OK {
+			DomPanic( dr, "UnuseElement" );
 		}
 	}
 }
