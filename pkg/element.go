ddba5a1ceff4c69411c0d831dffcc759cd50d08f
Eric Roy <ericsroy@gmail.com>
2011-12-31 18:14:05 -0800

    Modifications made to htmlayout.h so that it can be compiled with gcc, cgo successfuly compiling element.go

--- pkg/element.go
+++ pkg/element.go
@@ -1,14 +1,13 @@
 package htmlayout
 /*
-#cgo CFLAGS: -std=gnu89 -I../htmlayout/include
-//#cgo LDFLAGS: -l../htmlayout/lib/HTMLayout.lib
+#cgo CFLAGS: -I../htmlayout/include
+#cgo LDFLAGS: ../htmlayout/lib/HTMLayout.lib
 #include <htmlayout.h>
 */
 import "C"
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 const (
@@ -18,57 +17,45 @@ const (
 	HLDOM_PASSIVE_HANDLE = C.HLDOM_PASSIVE_HANDLE
 	HLDOM_INVALID_PARAMETER = C.HLDOM_INVALID_PARAMETER
 	HLDOM_OPERATION_FAILED = C.HLDOM_OPERATION_FAILED
-	HLDOM_OK_NOT_HANDLED = -1
+	HLDOM_OK_NOT_HANDLED = C.int(-1)
 )
 
-func init() {
-}
-
-
-type DomResult int
-
-var errorToString = map[DomResult]string {
-	HLDOM_OK: "HLDOM_OK",
-	HLDOM_INVALID_HWND: "HLDOM_INVALID_HWND",
-	HLDOM_INVALID_HANDLE: "HLDOM_INVALID_HANDLE",
-	HLDOM_PASSIVE_HANDLE: "HLDOM_PASSIVE_HANDLE",
-	HLDOM_INVALID_PARAMETER: "HLDOM_INVALID_PARAMETER",
-	HLDOM_OPERATION_FAILED: "HLDOM_OPERATION_FAILED",
-	HLDOM_OK_NOT_HANDLED: "HLDOM_OK_NOT_HANDLED",
+var errorToString = map[C.HLDOM_RESULT]string {
+	C.HLDOM_OK: "HLDOM_OK",
+	C.HLDOM_INVALID_HWND: "HLDOM_INVALID_HWND",
+	C.HLDOM_INVALID_HANDLE: "HLDOM_INVALID_HANDLE",
+	C.HLDOM_PASSIVE_HANDLE: "HLDOM_PASSIVE_HANDLE",
+	C.HLDOM_INVALID_PARAMETER: "HLDOM_INVALID_PARAMETER",
+	C.HLDOM_OPERATION_FAILED: "HLDOM_OPERATION_FAILED",
+	C.HLDOM_OK_NOT_HANDLED: "HLDOM_OK_NOT_HANDLED",
 }
 
 type DomError struct {
-	Result DomResult
+	Result C.HLDOM_RESULT
 	Message string
 }
 
 func (self DomError) String() string {
-	fmt.Sprintf( "%s: %s", errorToString[self.Result], self.Message )
+	return fmt.Sprintf( "%s: %s", errorToString[self.Result], self.Message )
 }
 
-func DomPanic(result DomResult, message string) {
+func DomPanic(result C.HLDOM_RESULT, message string) {
 	panic(DomError{result, message})
 }
 
 
-/*
-Handle
 
-Simple wrapper around an HELEMENT, provides reference 
-counting functionality
-*/
+type Handle C.HELEMENT
 
-type Handle unsafe.Pointer
-
-func (self Handle) use() {
-	if dr := C.HTMLayout_UseElement( self ); dr != HLDOM_OK {
+func use(handle Handle) {
+	if dr := C.HTMLayout_UseElement( handle ); dr != HLDOM_OK {
 		DomPanic( dr, "UseElement" );
 	}
 }
 
-func (self Handle) unuse() {
-	if self {
-		if dr := C.HTMLayout_UnuseElement( self ); dr != HLDOM_OK {
+func unuse(handle Handle) {
+	if handle != nil {
+		if dr := C.HTMLayout_UnuseElement( handle ); dr != HLDOM_OK {
 			DomPanic( dr, "UnuseElement" );
 		}
 	}
